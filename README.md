b70adc6bdc981b1fb395a3c5444ebb9502982600
isayme <isaymeorg@gmail.com>
2014-08-02 10:25:57 +0800

    modify to workd fine, and add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+这是一个Google镜像服务程序, 运行在[Node.js](http://nodejs.org/)环境, 使用[Express](http://expressjs.com/)框架.
+
+## 原理 ##
+使用[Google Custom Search](https://www.google.com/cse)的API.  
+看到[google200](https://github.com/zjuyxy/google200)使用此方法, 借鉴一下.
+
+
+## 使用方法 ##
+1. 首先确保你已经安装了[Node.js](http://nodejs.org/);
+2. 解压后, 执行`npm install`安装依赖库;
+3. `node app.js`开启服务.
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express();
+var google = express();
 var favicon = require('serve-favicon');
 var request = require('request');
 var fs = require('fs');
@@ -14,41 +14,42 @@ var REQ_URL = 'https://www.googleapis.com/customsearch/v1element?' +
                 '&prettyPrint=false' +
                 '&q=';
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+google.set('view engine', 'ejs');
+google.set('views', __dirname + '/views');
 
-app.use(function(req, res, next) {
-  console.log('%s %s', req.method, req.url);
-  next();
-});
+google.use(favicon(__dirname + '/public/img/favicon.ico'));
+google.use(express.static(__dirname + '/public/'));
 
-app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(express.static(__dirname + '/public/'));
-
-app.get('/search', function(req, res, next) {
+google.get('/search', function(req, res, next) {
   var url;
   if (req.query.q) {
     url = REQ_URL + req.query.q;
     if (req.query.start) url = url + '&start=' + req.query.start;
     
     request(url, function (err, response, body) {
+      var para;
+
       if (!err && response.statusCode == 200) {
-        res.render('search', JSON.parse(body));
+        para = JSON.parse(body);
+        if (para.error) {
+          res.render('index');
+        } else {
+          para.q = req.query.q;
+          res.render('search', para);
+        }
+        
       } else {
-        res.sendfile(__dirname + '/public/index.html');
+        res.render('index');
       }
     });
-    /*
-    var body = fs.readFileSync('./test.json');
-    res.render('search', JSON.parse(body));
-    */
+
   } else {
-    res.sendfile(__dirname + '/public/index.html');
+    res.render('index');
   }
 });
 
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/public/index.html');
+google.get('/', function(req, res) {
+  res.render('index');
 });
 
-app.listen(80);
+google.listen(80);
